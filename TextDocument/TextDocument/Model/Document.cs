ec0427a6c20d464303ed9385ffcff767964d313c
LalaMamedova <lallol606@gmail.com>
2023-03-31 20:16:02 +0400

    Text Documents

---
+++ TextDocument/TextDocument/Model/Document.cs
@@ -0,0 +1,55 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TextDocument.Model
+{
+    public class Document : ICloneable, INotifyPropertyChanged
+    {
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string name = "Новый Документ";
+        private int frontSize = 15;
+        private string content = string.Empty;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (value.Length <= 25)
+                    name = value;
+                else
+                    MessageBox.Show("Название слишком длинное");
+            }
+        }
+
+        public string Content { get => content; set { content = value;} }
+
+        [AlsoNotifyFor("FrontSize")]
+        public int FrontSize { get => frontSize; set { frontSize = value; } }
+        public bool IsRedacted { get; set; }
+        public static int _id { get; set; } = 0;
+        public int ID { get; set; } = _id;
+        public Document()
+        {
+            _id += 1;
+        }
+
+        public object Clone() => new Document()
+        {
+            FrontSize = this.FrontSize,
+        };
+
+
+
+    }
+}
---
+++ TextDocument/TextDocument/View/MainView.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TextDocument.Model;
+
+namespace TextDocument.View
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public int SelectedIndex { get; set; }
+        public ObservableCollection<Document> Documents { get; set; } = new();
+        public Document Document { get; set; } = new();
+        private bool IsPrevDelete = false;
+
+        public MainView()
+        {
+            InitializeComponent();
+            DocumentControl.ItemsSource = Documents;
+            Documents.Add(Document);
+        }
+
+     
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var NewDocument = (Document)Document.Clone();
+            Documents.Add(NewDocument);
+            MainTextBox.Text = NewDocument.Content;
+            SelectedIndex+=1;
+        }
+
+        private void RedactButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            SelectedIndex = (int)button.CommandParameter;
+
+            foreach (var docs in Documents)
+            {
+                if (docs.ID == SelectedIndex)
+                {
+                    FonSizeBox.Text = docs.FrontSize.ToString();
+                    MainTextBox.Text = docs.Content;
+                    docs.IsRedacted = true;
+                }
+            }
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            SelectedIndex = (int)button.CommandParameter;
+
+            foreach (var docs in Documents)
+            {
+                if(docs.ID == SelectedIndex)
+                {
+                    Documents.Remove(docs);
+
+                    for (int i = SelectedIndex; i < Documents.Count; i++)
+                        Documents[i].ID = i;
+                    
+                    Document._id = Documents.Count;
+                    break;
+                    
+                }
+            }
+        }
+
+
+        private void MainTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(MainTextBox.Text))
+            {
+                Documents[SelectedIndex].Content = MainTextBox.Text;
+
+            }
+        }
+
+     
+    }
+}
