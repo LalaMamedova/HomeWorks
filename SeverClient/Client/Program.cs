f32ab564174599de43a14aed4b657ddc2e67fb74
LalaMamedova <lallol606@gmail.com>
2023-07-17 19:34:30 +0400

    First network HW

---
+++ SeverClient/Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+try
+{
+    TcpClient client = new TcpClient("127.0.0.1",8080);
+
+    NetworkStream networkStream = client.GetStream();
+    byte[] bytes = new byte[256];
+    networkStream.Read(bytes, 0, bytes.Length);
+    string message = Encoding.ASCII.GetString(bytes);
+    Console.WriteLine(message);
+
+    StreamWriter writer = new StreamWriter(client.GetStream());
+    string message2 = "Bye, server!";
+    writer.WriteLine(message2);
+    writer.Close();
+    client.Close();
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
---
+++ SeverClient/SeverClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+TcpListener server = new(IPAddress.Any, 8080);
+
+try
+{
+   
+    server.Start(); Console.WriteLine("Welcome to this programm");
+    int i = 1;
+
+    while (true)
+    {
+        TcpClient client = server.AcceptTcpClient();
+
+        PostMessage(client);
+        GetMessage(i,client);
+        client.Close();
+        i++;
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Bye bye");
+    server.Stop();
+}
+
+
+void PostMessage(TcpClient client)
+{
+    NetworkStream stream = client.GetStream();
+    byte[] buffer = Encoding.ASCII.GetBytes("SOMEBODY ONCE TOLD ME");
+    stream.Write(buffer, 0, buffer.Length);
+}
+
+void GetMessage(int i, TcpClient client)
+{
+    StreamReader reader = new StreamReader(client.GetStream());
+    string message = reader.ReadLine();
+    Console.WriteLine($"Received message from client: {message}\n" +
+                      $"Client quit this programm {i} time ");
+}
