eab9f2182a6d9f5deccf10ab5d64109a7d4f1281
LalaMamedova <lallol606@gmail.com>
2023-07-27 13:26:11 +0400

    FTP

---
+++ FTPWpf/FTPWpf/FtpFileInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPWpf
+{
+    public class FtpFileInfo
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+    }
+}
--- FTPWpf/FTPWpf/MainWindow.xaml.cs
+++ FTPWpf/FTPWpf/MainWindow.xaml.cs
@@ -1,21 +1,16 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using Ookii.Dialogs.Wpf;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Windows.Shapes;
+using System.Drawing;
+using Cake.Core.IO;
 
 namespace FTPWpf
 {
@@ -25,31 +20,45 @@ namespace FTPWpf
     public partial class MainWindow : Window
     {
         FtpWebRequest request;
-        public ObservableCollection<string> FilesName { get; set; } = new();
+        bool isConnect = false;
+
+        public ObservableCollection<FtpFileInfo> FilesInfo { get; set; } = new();
         public string IP { get; set; } = "192.168.2.8";
-        public string Res { get; set; }
         public string DownloadPath { get; set; }
         public string FileName { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
-           
         }
 
 
+        public void ParseListing(string listing)
+        {
+            //Content = "{Binding Path=DataContext.Res,ElementName = Main}"
+ 
+            string[] lines = listing.Split(new[] { "\r\n", "\n"}, StringSplitOptions.RemoveEmptyEntries);
+          
+            foreach (string line in lines)
+            {
+                string[] oneLine = line.Split(new[] { "\t"," " }, StringSplitOptions.RemoveEmptyEntries);
+
+                string name = oneLine[3];
+                long size = long.Parse(oneLine[2]);
+                if (oneLine.Length > 4) name += oneLine[4];
+
+                FilesInfo.Add(new FtpFileInfo { Name = name, Size = size / 1024 });
+            }
+        }
 
         private string? Response()
         {
-            request.Method = WebRequestMethods.Ftp.ListDirectory;
-
             try
             {
                 FtpWebResponse response = (FtpWebResponse)request.GetResponse();
 
                 Stream responseStream = response.GetResponseStream();
                 StreamReader reader = new StreamReader(responseStream);
-
                 return reader.ReadToEnd();
             }
             catch (Exception ex)
@@ -64,22 +73,21 @@ namespace FTPWpf
 
  
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(IP))
+
+            if (!string.IsNullOrEmpty(IP) && isConnect==false)
             {
                 try
                 {
                     request = (FtpWebRequest)WebRequest.Create($"ftp://{IP}");
-                    string allResponse = Response();
+                    request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
 
-                    if (FilesName.Count > 0)
-                    {
-                        FilesName.Clear();
-                        Res = "";
-                    }
-                    FilesName.Add(allResponse);
-                    Res = allResponse;
+                    string allResponse = Response();
+                    isConnect = true;
+
+                    
+                    ParseListing(allResponse);
 
                 }
                 catch (Exception ex)
@@ -93,30 +101,82 @@ namespace FTPWpf
 
         private void DownLoadButton_Click(object sender, RoutedEventArgs e)
         {
-            request = (FtpWebRequest)WebRequest.Create($"ftp://{IP}/{FileName}");
-            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            if (isConnect == true)
+            {
+                request = (FtpWebRequest)WebRequest.Create($"ftp://{IP}/{FileName}");
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
 
-            using FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-            using Stream stream = response.GetResponseStream();
-            using FileStream fileStream = new($"{DownloadPath}/{FileName}", FileMode.Create);
-            stream.CopyTo(fileStream);
+                using FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                using Stream stream = response.GetResponseStream();
+
+                using FileStream fileStream = new($"{DownloadPath}/{FileName}", FileMode.Create);
+                stream.CopyTo(fileStream);
+            }
+            else
+                MessageBox.Show("Подключитесь в начале");
         }
 
         private void AddButton(object sender, RoutedEventArgs e)
         {
-            var fileDialog = new OpenFileDialog();
-            fileDialog.ShowDialog();
+            if (isConnect == true)
+            {
+                var fileDialog = new OpenFileDialog();
+                fileDialog.ShowDialog();
 
-            request = (FtpWebRequest)WebRequest.Create($"ftp://{IP}/{fileDialog.SafeFileName}");
-            request.Method = WebRequestMethods.Ftp.AppendFile;
+                request = (FtpWebRequest)WebRequest.Create($"ftp://{IP}/{fileDialog.SafeFileName}");
+                request.Method = WebRequestMethods.Ftp.AppendFile;
 
-            using FileStream fileStream = new FileStream(fileDialog.FileName, FileMode.Open);
+                using FileStream fileStream = new FileStream(fileDialog.FileName, FileMode.Open);
 
-            byte[] fileContents = new byte[fileStream.Length];
-            fileStream.Read(fileContents, 0, fileContents.Length);
+                byte[] fileContents = new byte[fileStream.Length];
+                fileStream.Read(fileContents, 0, fileContents.Length);
 
-            using Stream requestStream = request.GetRequestStream();
-            requestStream.Write(fileContents, 0, fileContents.Length);
+                using Stream requestStream = request.GetRequestStream();
+                requestStream.Write(fileContents, 0, fileContents.Length);
+            }
+            else
+                MessageBox.Show("Подключитесь в начале");
+
+        }
+
+        private void ChoiceButton(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog folder = new();
+            folder.ShowDialog();
+            DownloadPath = folder.SelectedPath;
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            request = (FtpWebRequest)WebRequest.Create($"ftp://{IP}");
+
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            string allResponse = Response();
+
+            try
+            {
+                if (FilesInfo.Count > 0)
+                {
+                    FilesInfo.Clear();
+                    ParseListing(allResponse);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            request = (FtpWebRequest)WebRequest.Create($"ftp://{IP}/{FileName}");
+            request.Method = WebRequestMethods.Ftp.DeleteFile;
+
+            using FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            using Stream stream = response.GetResponseStream();
+
             
         }
     }
